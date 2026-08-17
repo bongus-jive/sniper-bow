f3439c31188dcb6377b5069caa88048c9c3cbd67
patmanf <27874300+patmanf@users.noreply.github.com>
2026-08-17 18:04:53 -0500

    redo abilities

---
+++ pat/sniperbow/items/sniperbowshot.lua
@@ -0,0 +1,30 @@
+require "/items/active/weapons/bow/abilities/bowshot.lua"
+
+SniperBowShot = setmetatable({}, { __index = BowShot })
+
+function SniperBowShot:fire()
+  if self.doMuzzleflash ~= false then
+    animator.setPartTag("muzzleFlash", "variant", math.random(3))
+    animator.setAnimationState("muzzleFlash", "fire")
+    animator.burstParticleEmitter("muzzleFlash")
+    animator.playSound("fire")
+    animator.playSound("fire2")
+  end
+
+  BowShot.fire(self)
+end
+
+function SniperBowShot:currentProjectileParameters()
+  local perfect = self:perfectTiming()
+  local cfg = root.projectileConfig(perfect and self.powerProjectileType or self.projectileType)
+  local params = sb.jsonMerge(self.projectileParameters or {}, perfect and self.powerProjectileParameters or {})
+
+  params.speed = (params.speed or cfg.speed) * root.evalFunction(self.drawSpeedMultiplier, self.drawTime)
+  params.powerMultiplier = activeItem.ownerPowerMultiplier() * self.weapon.damageLevelMultiplier * root.evalFunction(self.drawPowerMultiplier, self.drawTime)
+
+  if self.flipParameters and mcontroller.facingDirection() == -1 then
+    params = sb.jsonMerge(params, self.flipParameters)
+  end
+
+  return params
+end
